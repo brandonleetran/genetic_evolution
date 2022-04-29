52880adc5493ee72f237096d9e345889d98ed2d6
brandonleetran <brandonleetran@icloud.com>
2022-04-29 14:47:30 -0500

    Calculated fitness scores for each DNA.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,57 @@
 // the interface for our DNA objects
 interface DNA {
     word : string,
+    fitness?: number // this is optional for the initial population
 }
 
 // the target value
-const target = ""
+const target = "brandon tran" 
+
+// the max population
+const MAX_POPULATION = 10
+
+// the minimum population
+const MIN_POPULATION = 1
 
 // the population of our generation (the higher the population, the higher the variance)
-const POPULATION = 100
+const POPULATION = randomNumber(MAX_POPULATION, MIN_POPULATION)
 
 // the character set of which we are using (note: I am not using integer values)
-const charset = "abcdefghijklmnopqrstuvwxyz"
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 1234567890"
 
 // the list of DNAs that will be generated using natural selection
-const array = [] as DNA[]
+let array: DNA[] = []
 
 // program starts here
 function init() {
 
-    let stringLength = 5
+    let stringLength = target.length
+
+    // populating the initial generation
     for (let i = 0; i < POPULATION; i++)
     {
-        let obj = {word: randomString(stringLength)} as DNA
+        // create a DNA object
+        let obj = {} as DNA
+
+        // create a random word based on string length
+        let word = randomString(stringLength)
+
+        obj.word = word
+
         array.push(obj)
+        array = calcFitness(array)
     }
 
-    array.forEach((DNA) => {
-        console.log(DNA.word)
-    })
+    // print
+    console.log(array)
 }
 
-// pure function
+// pure function that returns a random integer between min (inclusive) and max (inclusive).
+function randomNumber(max : number, min : number ) : number {
+    return Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min)
+}
+
+// pure function that returns the length of a DNA string
 function stringLength(obj : DNA) : number {
     return obj.word.length
 }
@@ -40,11 +61,43 @@ function randomString(stringLength : number) : string {
     let result = ''
     for (let i = 0; i < stringLength; i++)
     {
+        // generates a random string based on the charset we are using
         result += charset[Math.floor(Math.random() * charset.length)]
     }
     return result
 }
 
+// function that calculates the fitness score of each DNA object
+function calcFitness(obj : DNA[]) : DNA[] {
+
+    const array: DNA[] = []
+
+    // calculate fitness score here
+    obj.forEach((DNA => {
+        let word = DNA.word
+
+        // we know all strings populated will have the same length as 
+        // the target
+        let score = 0
+
+        for (let i = 0; i < word.length; i++) {
+            // we are including white space as well
+            if (word.charAt(i) === target.charAt(i)) {
+                score += 1
+            }
+        }
+
+        // add fitnesss score to array
+        array.push({
+            word: word,
+            fitness: score
+        })
+    }))
+
+    return array
+}
+
+// start
 init()
 
-console.log("end")
+console.log('The population was ' + POPULATION)
